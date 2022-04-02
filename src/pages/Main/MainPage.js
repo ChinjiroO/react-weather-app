223fe45979dd11fa455b931bd6874fbf1596cd38
ChinjiroO <techinseoikrathok@gmail.com>
2022-04-02 22:46:13 +0700

    Fix: 400 bad request.

--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  CurrentWeather as CurrentWeathers,
-  Forecast as Forecasts,
-} from "../../components";
+import { CurrentWeather, Forecast } from "../../components";
 import styles from "./MainPage.module.css";
 
 const MainPage = () => {
@@ -22,24 +19,26 @@ const MainPage = () => {
 
   // Fetch Openweathermap
   useEffect(() => {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
-    )
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw res;
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    if (lat && lon) {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          setData(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
   }, [lat, lon]);
 
   // Set Loading...
@@ -47,19 +46,17 @@ const MainPage = () => {
     return "Loading...";
   }
 
-  console.log(data);
-
   return (
     <div className={styles.containers}>
       <h1>Weather forecast</h1>
       {/* Card */}
-      <CurrentWeathers
+      <CurrentWeather
         temp={data !== null ? data.current.temp : "..."}
         feelsLike={data !== null ? data.current.feels_like : "..."}
         weather={data !== null ? data.current.weather[0].description : "..."}
         dt={data !== null ? Date(data.current.dt.toLocaleString()) : "..."}
       />
-      <Forecasts />
+      <Forecast />
     </div>
   );
 };
